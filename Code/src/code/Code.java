6845b0ce51e66e02a82b8be55bbe2bc30472ed60
reginamjr.14 <reginamjr.14@Regina>
2016-08-26 11:57:40 -0500

    En este commit estoy creando un metodo paa crear una calculadora

--- Code/src/code/Code.java
+++ Code/src/code/Code.java
@@ -5,6 +5,8 @@
  */
 package code;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reginamjr.14
@@ -15,10 +17,29 @@ public class Code {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner var = new Scanner (System.in);
         // TODO code application logic here
         System.out.println("Universidad Laica Eloy Alfaro de Manabí ==FACCI===");
         System.out.println("Hola Profesor este es un deber de Ingenieria del Software II");
         System.out.println("Realizado por Regina Vera Ramirez ");
+        if (var.nextLine().equals("1")){
+            
+        }
+        
     }
+    
+    public void calculadora(){
+        
+    Scanner mientrada = new  Scanner(System.in);
+        String suma="1";// para las opciones
+        String resta="2";
+        String multiplicar="3";
+        String Dividir="4";
+        String opcion="";
+   
+        
+
+}
+    
     
 }

74935690886a0a0eafbcfbb7373e1240c2f64c3f
reginamjr.14 <reginamjr.14@Regina>
2016-08-26 00:50:39 -0500

    En este commit se muestra un mensaje de presentacion

---
+++ Code/src/code/Code.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package code;
+
+/**
+ *
+ * @author reginamjr.14
+ */
+public class Code {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Universidad Laica Eloy Alfaro de Manabí ==FACCI===");
+        System.out.println("Hola Profesor este es un deber de Ingenieria del Software II");
+        System.out.println("Realizado por Regina Vera Rmairez ");
+    }
+    
+}
